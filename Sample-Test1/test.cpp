65451f01102d4a85b8f12669a32f09f274178077
kirim <kirim.park@naver.com>
2023-06-01 15:14:55 +0900

    [feature&refactoring] 35p

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
+#include "../primeFactors/PrimeFactor.cpp"
 
+using namespace std;
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
