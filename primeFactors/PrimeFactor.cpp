36dc671812c11d7893527131b8ffe9500f96423b
kirim <kirim.park@naver.com>
2023-06-01 15:46:24 +0900

    [refactoring] 42p

--- primeFactors/PrimeFactor.cpp
+++ primeFactors/PrimeFactor.cpp
@@ -4,16 +4,12 @@ using namespace std;
 class PrimeFactor
 {
 public:
-	vector<int> of(int i)
+	vector<int> of(int number)
 	{
 		vector<int> result = {};
-		if (i == 2)
+		if (number > 1)
 		{
-			result.push_back(2);
-		}
-		if (i == 3)
-		{
-			result.push_back(3);
+			result.push_back(number);
 		}
 		return result;
 	}
