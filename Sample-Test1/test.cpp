24a923a36e39c01aae378ca662ca2471ebbc797d
kirim <kirim.park@naver.com>
2023-06-01 15:42:00 +0900

    [feature] 37p

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,8 +2,13 @@
 #include "../primeFactors/PrimeFactor.cpp"
 
 using namespace std;
-TEST(TestCaseName, TestName) {
+TEST(TestCaseName, of1) {
 	PrimeFactor prime_factor;
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
+}
+TEST(TestCaseName, of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
--- primeFactors/PrimeFactor.cpp
+++ primeFactors/PrimeFactor.cpp
@@ -7,6 +7,10 @@ public:
 	vector<int> of(int i)
 	{
 		vector<int> result = {};
+		if (i == 2)
+		{
+			result.push_back(2);
+		}
 		return result;
 	}
 };
