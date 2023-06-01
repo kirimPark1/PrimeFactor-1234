7c19e88264c674d64a84ca34d7e717140c08d37f
kirim <kirim.park@naver.com>
2023-06-01 15:47:39 +0900

    [feature] 44p

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -20,3 +20,7 @@ TEST_F(PrimeFixture, of3) {
 	expected = {3};
 	EXPECT_EQ(expected, prime_factor.of(3));
 }
+TEST_F(PrimeFixture, of4) {
+	expected = { 2, 2};
+	EXPECT_EQ(expected, prime_factor.of(4));
+}
